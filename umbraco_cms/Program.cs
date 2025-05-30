d26277f7ab45ecc20200ae3569a93145833cedc0
Kristian Flamsted <mfsboy@msn.com>
2025-05-30 22:34:38 +0200

    Setting up Nikcio.UHeadless package

--- umbraco_cms/Program.cs
+++ umbraco_cms/Program.cs
@@ -1,15 +1,47 @@
+using HotChocolate.AspNetCore;
+using HotChocolate.AspNetCore.Extensions;
+using Nikcio.UHeadless;
+using Nikcio.UHeadless.Defaults.ContentItems;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// .AddBackOffice()
+// .AddWebsite()
+// .AddComposers()
 
 builder.CreateUmbracoBuilder()
     .AddBackOffice()
     .AddWebsite()
     .AddComposers()
+    .AddUHeadless(options =>
+    {
+        options.DisableAuthorization = true; // Change this later when adding authentication - See documentation
+        options.AddDefaults();
+        options.AddQuery<ContentByRouteQuery>();
+        options.AddQuery<ContentByGuidQuery>();
+    })
     .Build();
 
 WebApplication app = builder.Build();
 
 await app.BootUmbracoAsync();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
+GraphQLEndpointConventionBuilder graphQLEndpointBuilder = app.MapUHeadless();
+
+// Only enable the GraphQL IDE in development
+if (!builder.Environment.IsDevelopment())
+{
+    graphQLEndpointBuilder.WithOptions(new GraphQLServerOptions()
+    {
+        Tool =
+        {
+            Enable = false,
+        }
+    });
+}
 
 app.UseUmbraco()
     .WithMiddleware(u =>
