e92a0dbd4f30d00d5185887fee189528e4c977ca
Kristian Flamsted <mfsboy@msn.com>
2025-06-01 16:57:41 +0200

    Removing uneccesary exports

--- content_creator_client/src/models/common/ImageCropperValue.ts
+++ content_creator_client/src/models/common/ImageCropperValue.ts
@@ -2,19 +2,19 @@
  * Represents the value structure of Umbraco's Image Cropper property editor.
  * This is a minimal version; extend as needed for your use case.
  */
-export interface FocalPoint {
+interface FocalPoint {
   left: number;
   top: number;
 }
 
-export interface Coordinates {
+interface Coordinates {
   x1: number;
   y1: number;
   x2: number;
   y2: number;
 }
 
-export interface Crop {
+interface Crop {
   alias: string;
   width: number;
   height: number;
