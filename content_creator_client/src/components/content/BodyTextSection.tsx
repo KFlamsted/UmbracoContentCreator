508504eb8052eb22b79ae90760734eed07efd5a4
Kristian Flamsted <kristian.flamsted@keylane.com>
2025-07-23 21:04:21 +0200

    BodyTextSection and page title section updated to use the new tailwindcss components instead of classes

--- content_creator_client/src/components/content/BodyTextSection.tsx
+++ content_creator_client/src/components/content/BodyTextSection.tsx
@@ -1,5 +1,6 @@
 import parse from 'html-react-parser'
-import { BODY_TEXT_CLASSES, SECTION_SPACING } from '../../constants/styles'
+import { SectionComponent } from '../ui'
+import { DESIGN_TOKENS } from '../../constants/styles'
 
 interface BodyTextSectionProps {
   /**
@@ -13,12 +14,19 @@ const BodyTextSection: React.FC<BodyTextSectionProps> = ({
   id,
   bodyText
 }) => {
+  if (!bodyText) return null
+
+  const richTextClasses = `${DESIGN_TOKENS.BODY_SIZE} ${DESIGN_TOKENS.TEXT_BODY} prose prose-lg max-w-none [&_ul]:list-disc [&_ul]:pl-6 [&_ol]:list-decimal [&_ol]:pl-6`
+
   return (
-    <div id={id} className={SECTION_SPACING}>
-      <div id={`${id}-content`} className={BODY_TEXT_CLASSES}>
-        {bodyText ? parse(bodyText) : ''}
+    <SectionComponent id={id} variant="card-section" spacing="default">
+      <div 
+        id={`${id}-content`} 
+        className={richTextClasses}
+      >
+        {parse(bodyText)}
       </div>
-    </div>
+    </SectionComponent>
   )
 }
 
